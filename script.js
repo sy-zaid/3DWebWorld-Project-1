d3bfbe4513c39c9217aea7b6e7702537d7e63efa
SyedZaid <syedmzaid.99@gmail.com>
2023-09-14 20:03:27 +0500

    Commit-2

--- script.js
+++ script.js
@@ -67,9 +67,9 @@ gsap.to("#page-2>h1>span", {
 });
 
 // PAGE - 3 | Javascript
-function canvas() {
-  const canvas = document.querySelector("#page-3>canvas");
-  const context = canvas.getContext("2d");
+function creating_canvas() {
+  let canvas = document.querySelector("canvas");
+  let context = canvas.getContext("2d");
 
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
@@ -80,129 +80,134 @@ function canvas() {
     render();
   });
 
-  function files(index) {
-    var data = `
-    static/frames00013.png
-    static/frames00010.png
-    static/frames00016.png
-    static/frames00019.png
-    static/frames00022.png
-    static/frames00025.png
-    static/frames00028.png
-    static/frames00031.png
-    static/frames00034.png
-    static/frames00037.png
-    static/frames00040.png
-    static/frames00043.png
-    static/frames00046.png
-    static/frames00049.png
-    static/frames00052.png
-    static/frames00055.png
-    static/frames00058.png
-    static/frames00061.png
-    static/frames00064.png
-    static/frames00067.png
-    static/frames00070.png
-    static/frames00073.png
-    static/frames00076.png
-    static/frames00079.png
-    static/frames00082.png
-    static/frames00085.png
-    static/frames00088.png
-    static/frames00091.png
-    static/frames00094.png
-    static/frames00097.png
-    static/frames00100.png
-    static/frames00103.png
-    static/frames00106.png
-    static/frames00109.png
-    static/frames00112.png
-    static/frames00115.png
-    static/frames00118.png
-    static/frames00121.png
-    static/frames00124.png
-    static/frames00127.png
-    static/frames00130.png
-    static/frames00133.png
-    static/frames00136.png
-    static/frames00139.png
-    static/frames00142.png
-    static/frames00145.png
-    static/frames00148.png
-    static/frames00151.png
-    static/frames00154.png
-    static/frames00157.png
-    static/frames00160.png
-    static/frames00163.png
-    static/frames00166.png
-    static/frames00169.png
-    static/frames00172.png
-    static/frames00175.png
-    static/frames00178.png
-    static/frames00181.png
-    static/frames00184.png
-    static/frames00187.png
-    static/frames00190.png
-    static/frames00193.png
-    static/frames00196.png
-    static/frames00199.png
-    static/frames00202.png
-    static/frames00007.png
-`;
-    return data.split("\n")[index];
-  }
+  let frameno = 67;
 
-  const frameCount = 67;
-
-  const images = [];
-  const imageSeq = {
+  let imagesList = [];
+  let imageSeq = {
     frame: 1,
   };
 
-  for (let i = 0; i < frameCount; i++) {
-    const img = new Image();
-    img.src = files(i);
-    images.push(img);
+  let sourceOfImages = `
+static/frames00013.png
+static/frames00010.png
+static/frames00016.png
+static/frames00019.png
+static/frames00022.png
+static/frames00025.png
+static/frames00028.png
+static/frames00031.png
+static/frames00034.png
+static/frames00037.png
+static/frames00040.png
+static/frames00043.png
+static/frames00046.png
+static/frames00049.png
+static/frames00052.png
+static/frames00055.png
+static/frames00058.png
+static/frames00061.png
+static/frames00064.png
+static/frames00067.png
+static/frames00070.png
+static/frames00073.png
+static/frames00076.png
+static/frames00079.png
+static/frames00082.png
+static/frames00085.png
+static/frames00088.png
+static/frames00091.png
+static/frames00094.png
+static/frames00097.png
+static/frames00100.png
+static/frames00103.png
+static/frames00106.png
+static/frames00109.png
+static/frames00112.png
+static/frames00115.png
+static/frames00118.png
+static/frames00121.png
+static/frames00124.png
+static/frames00127.png
+static/frames00130.png
+static/frames00133.png
+static/frames00136.png
+static/frames00139.png
+static/frames00142.png
+static/frames00145.png
+static/frames00148.png
+static/frames00151.png
+static/frames00154.png
+static/frames00157.png
+static/frames00160.png
+static/frames00163.png
+static/frames00166.png
+static/frames00169.png
+static/frames00172.png
+static/frames00175.png
+static/frames00178.png
+static/frames00181.png
+static/frames00184.png
+static/frames00187.png
+static/frames00190.png
+static/frames00193.png
+static/frames00196.png
+static/frames00199.png
+static/frames00202.png
+static/frames00007.png
+`;
+  sourceOfImages = sourceOfImages.split("\n");
+
+  function set_image_list(sList, fCount) {
+    /*This function sets up the empty array into an array of images along with their paths*/
+
+    for (let i = 0; i < fCount; i++) {
+      let newImage = new Image();
+      newImage.src = sourceOfImages[i];
+      sList.push(newImage);
+    }
   }
 
+  set_image_list(imagesList, frameno);
+  // console.log(imagesList)
+
   gsap.to(imageSeq, {
-    frame: frameCount - 1,
-    snap: "frame",
-    ease: `none`,
+    frame: frameno - 1,
+    snap: `frame`,
+    ease: `Power4.easeOut`,
     scrollTrigger: {
-      scrub: 0.2,
+      scrub: .5,
+      scroller: "#main",
       trigger: `#page-3`,
       start: `top top`,
       end: `250% top`,
-      scroller: `#main`,
     },
     onUpdate: render,
   });
 
-  images[1].onload = render;
+  imagesList[1].onload = render;
 
   function render() {
-    scaleImage(images[imageSeq.frame], context);
+    scaleImage(imagesList[imageSeq.frame], context);
   }
 
-  function scaleImage(img, ctx) {
-    var canvas = ctx.canvas;
-    var hRatio = canvas.width / img.width;
-    var vRatio = canvas.height / img.height;
-    var ratio = Math.max(hRatio, vRatio);
-    var centerShift_x = (canvas.width - img.width * ratio) / 2;
-    var centerShift_y = (canvas.height - img.height * ratio) / 2;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(
-      img,
+  function scaleImage(lImg, cont) {
+    let canvas = cont.canvas;
+    let hRatio = canvas.width / lImg.width;
+    let vRatio = canvas.height / lImg.height;
+    max_ratio = Math.max(hRatio, vRatio);
+    let centerX = (canvas.width - lImg.width * max_ratio) / 2;
+    let centerY = (canvas.height - lImg.height * max_ratio) / 2;
+    cont.clearRect(0, 0, canvas.width, canvas.height);
+    cont.drawImage(
+      lImg,
       0,
       0,
-      img.width,
-      img.height,
-      centerShift_x,
-      centerShift_y,
-      img.width * ratio,
-      img.height * ratio
+      lImg.width,
+      lImg.height,
+      centerX,
+      centerY,
+      lImg.width * max_ratio,
+      lImg.height * max_ratio
     );
   }
   ScrollTrigger.create({
@@ -210,7 +215,11 @@ function canvas() {
     pin: true,
     scroller: `#main`,
     start: `top top`,
-    end: `250% top`,
+    end: `bottom top`,
+    scrub:0.8
   });
 }
-canvas();
+
+creating_canvas();
+
+
